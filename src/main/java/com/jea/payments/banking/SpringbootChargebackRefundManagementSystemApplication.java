a3e9738cb8562beedf861c019d261e7e70223577
Jagadeesh <blockchian@jeadigital.com>
2024-11-15 17:54:35 +0530

    Initial commit

---
+++ src/main/java/com/jea/payments/banking/SpringbootChargebackRefundManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.jea.payments.banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootChargebackRefundManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootChargebackRefundManagementSystemApplication.class, args);
+	}
+
+}
